16217d6a78f58545345a5451b528a117958a0824
Agility76 <filiplewin99@gmail.com>
2021-04-21 15:45:59 +0200

    Dodaj pliki projektów.

---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kółko_i_krzyżyk
+{
+    public partial class Form1 : Form
+    {
+        enum CurrentPlayer {
+            Cross,
+            Circle
+        }
+        CurrentPlayer currentPlayer;
+        public Form1()
+        {
+            InitializeComponent();
+            currentPlayer = CurrentPlayer.Cross;
+            changeLabe();
+        }
+
+        private void tr_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mark(object sender, EventArgs e)
+        {
+            Button senderButton = (Button)sender;
+            if (currentPlayer == CurrentPlayer.Cross)
+            {
+                senderButton.Text = "X";
+                currentPlayer = CurrentPlayer.Circle;
+            }
+            else
+            {
+                senderButton.Text = "O";
+                currentPlayer = CurrentPlayer.Cross;
+            }
+            checkForWinner();
+            changeLabe();
+        }
+        public void changeLabe()
+        {
+            if(currentPlayer == CurrentPlayer.Cross)
+            {
+                currentPlayerLabel.Text = "Krzyżyk";
+            }
+            else
+            {
+                currentPlayerLabel.Text = "Kółko";
+            }
+        }
+        public void checkForWinner()
+        {
+            if(String.Compare(tl.Text, cl.Text) == 0 && String.Compare(cl.Text, bl.Text) == 0)
+            {
+                Form2 victoryscreen = new Form2(this);
+                victoryscreen.winner = tl.Text;
+                victoryscreen.Show();
+            }
+        }
+        public void clearBoard()
+        {
+            TableLayoutControlCollection buttons = tableLayoutPanel1.Controls;
+
+            for(int i = 0; i < buttons.Count; i++)
+            {
+                if (buttons[i] is Button)
+                    buttons[i].Text = "";
+            }
+            currentPlayer = CurrentPlayer.Cross; 
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace kółko_i_krzyżyk
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.winnerLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(25, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(728, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gratulacje winner winner chicken dinner";
+            // 
+            // winnerLabel
+            // 
+            this.winnerLabel.AutoSize = true;
+            this.winnerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.winnerLabel.Location = new System.Drawing.Point(25, 280);
+            this.winnerLabel.Name = "winnerLabel";
+            this.winnerLabel.Size = new System.Drawing.Size(195, 46);
+            this.winnerLabel.TabIndex = 1;
+            this.winnerLabel.Text = "Wygrywa:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(302, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ok to gramy znowu :)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.winnerLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.Shown += new System.EventHandler(this.showWinner);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label winnerLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
